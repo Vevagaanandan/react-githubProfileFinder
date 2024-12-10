1192f29a65d64812aed88cdf3fe716ff68c10ea8
Vevagaanandan Pillai <75841061+Vevagaanandan@users.noreply.github.com>
2024-12-10 22:40:50 +0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import GithubProfileFinder from './components/github-profile-finder';
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <GithubProfileFinder />
+      </div>
+      
+    </>
+  )
+}
+
+export default App
